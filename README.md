aa1827d9903911894900ff16fdacd068882465be
Daniel Diekmeier <fax@danieldiekmeier.de>
2016-04-30 19:17:42 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# TextExpander to Alfred 3
+
+*Note: Alfred 3 is currently in beta. Their format for snippets may change and this program may (temporarily) stop working as a result.*
+
+With [TextExpander](https://smilesoftware.com/textexpander) changing its business model, the new [Alfred](https://www.alfredapp.com/) version supporting text replacement, and the general wish to use the smallest number of different apps, I wrote this script to convert the `.textexpander` files to `.alfredsnippets` files.
+
+TextExpander uses a `plist` with all the entries in an array, and Alfred uses a ZIP file containing a JSON file for each snippet.
+
+## Installation
+
+I recommend you to do
+
+```
+npm install -g textexpander-to-alfred3
+```
+
+But you can, of course, do whatever you want.
+
+## Usage
+
+You just have to run the program with the `.textexpander` file as the only argument. It will then put a `.alfredsnippets` file in your current working directory.
+
+For example:
+
+```
+textexpander-to-alfred3 Signatures.textexpander
+> Wrote file /Users/You/Signatures.alfredsnippets
+```
+
+You can also use `te2a3` instead with the same result:
+
+```
+te2a3 Signatures.textexpander
+> Wrote file /Users/You/Signatures.alfredsnippets
+```
---
+++ textexpander-to-alfred3.js
@@ -0,0 +1,68 @@
+'use strict'
+
+const fs = require('fs')
+const path = require('path')
+const archiver = require('archiver')
+const sanitize = require('sanitize-filename')
+const plist = require('plist')
+const program = require('commander')
+
+program
+  .version('0.0.1')
+  .usage('<file>')
+  .option('<file>', '.textexpander source file')
+  .parse(process.argv)
+
+// show help by default
+if (!process.argv.slice(2).length) {
+  program.outputHelp()
+} else {
+  main()
+}
+
+function readPlist (path, callback) {
+  fs.readFile(path, 'utf-8', (err, data) => {
+    callback(err, plist.parse(data))
+  })
+}
+
+function transformSnippet (snippet) {
+  return {
+    filename: sanitize(snippet.abbreviation.slice(0, 10)) + ' [' + snippet.uuidString + '].json',
+    content: JSON.stringify({
+      alfredsnippet: {
+        snippet: snippet.plainText,
+        name: snippet.abbreviation.slice(0, 10),
+        uid: snippet.uuidString,
+        keyword: snippet.abbreviation
+      }
+    }, null, 2)
+  }
+}
+
+function main () {
+  readPlist(path.join(process.cwd(), program.args[0]), function (err, plist) {
+    if (err) { throw err }
+    const outFile = path.join(process.cwd(), (plist.groupInfo.groupName + '.alfredsnippets'))
+
+    const output = fs.createWriteStream(outFile)
+    const archive = archiver('zip')
+
+    archive.pipe(output)
+
+    output.on('close', function () {
+      console.log(`Wrote file ${outFile}`)
+    })
+
+    plist.snippetsTE2
+      .map(transformSnippet)
+      .forEach(function (snippet) {
+        archive.append(snippet.content, {
+          name: snippet.filename
+        })
+      })
+
+    archive
+      .finalize()
+  })
+}
