01a23c1507d4f3707f4ef2f566673a98216f9de5
Daniel Diekmeier <fax@danieldiekmeier.de>
2016-05-19 20:34:14 +0200

    Bump version number

--- textexpander-to-alfred3.js
+++ textexpander-to-alfred3.js
@@ -10,7 +10,7 @@ const plist = require('plist')
 const program = require('commander')
 
 program
-  .version('0.0.3')
+  .version('0.1.0')
   .usage('<file>')
   .option('<file>', '.textexpander source file')
   .parse(process.argv)
